ec1cb32adfc6da435666e7567db90c57b8a4b3b7
rabinadk1 <rabin.adk1@gmail.com>
2020-09-29 20:25:21 +0545

    Added event and venue registration routes and forms

--- covaplad/forms.py
+++ covaplad/forms.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from flask_wtf import FlaskForm
 from wtforms.fields import PasswordField, StringField, SubmitField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import EqualTo, Length, Optional
+from wtforms.fields.html5 import DateTimeLocalField, EmailField, IntegerField
+from wtforms.validators import DataRequired, EqualTo, Length, NumberRange, Optional
 
 
 class UserRegistrationForm(FlaskForm):
@@ -17,3 +19,37 @@ class UserRegistrationForm(FlaskForm):
     #     "Phone Number", validators=[NumberRange(100, 9999999999)]
     # )
     submit = SubmitField("Sign Up")
+
+
+class EventResistrationForm(FlaskForm):
+    name = StringField("Name", validators=[Length(2, 100)])
+    start = DateTimeLocalField(
+        "Start Date",
+        default=datetime.now,
+        validators=[
+            DataRequired(
+                "Invalid Datetime format. Please input in format ='%Y-%m-%d %H:%M:%S"
+            )
+        ],
+    )
+    end = DateTimeLocalField(
+        "End Date",
+        default=datetime.now,
+        validators=[
+            DataRequired(
+                "Invalid Datetime format. Please input in format ='%Y-%m-%d %H:%M:%S"
+            )
+        ],
+    )
+    address = StringField("Address", validators=[Length(2, 255)])
+    submit = SubmitField("Register Event")
+
+
+class DonationVenueRegistrationForm(FlaskForm):
+    name = StringField("Name", validators=[Length(2, 100)])
+    email = EmailField("Email", validators=[Length(max=255)])
+    address = StringField("Address", validators=[Optional(), Length(2, 255)])
+    phone_number = IntegerField(
+        "Phone Number", default=0, validators=[NumberRange(100, 9999999999)]
+    )
+    submit = SubmitField("Register Venue")
--- covaplad/routes.py
+++ covaplad/routes.py
@@ -1,8 +1,12 @@
 from flask import redirect, render_template, url_for
 
 from . import app, db
-from .forms import UserRegistrationForm
-from .models import User
+from .forms import (
+    DonationVenueRegistrationForm,
+    EventResistrationForm,
+    UserRegistrationForm,
+)
+from .models import DonationVenue, Event, User
 
 
 @app.route("/")
@@ -26,3 +30,35 @@ def register():
         db.session.commit()
         return redirect(url_for("index"))
     return render_template("register.html", form=form)
+
+
+@app.route("/register_event", methods=["GET", "POST"])
+def register_event():
+    form = EventResistrationForm()
+    if form.validate_on_submit():
+        event = Event(
+            name=form.name.data,
+            start=form.start.data,
+            end=form.end.data,
+            address=form.address.data,
+        )
+        db.session.add(event)
+        db.session.commit()
+        return redirect(url_for("register_event"))
+    return render_template("register_event.html", form=form)
+
+
+@app.route("/register_venue", methods=["GET", "POST"])
+def register_venue():
+    form = DonationVenueRegistrationForm()
+    if form.validate_on_submit():
+        venue = DonationVenue(
+            name=form.name.data,
+            email=form.email.data,
+            address=form.address.data,
+            phone_number=form.phone_number.data,
+        )
+        db.session.add(venue)
+        db.session.commit()
+        return redirect(url_for("register_venue"))
+    return render_template("register_venue.html", form=form)
