d8eead5565c01acc48f06afdc329409765ca6705
QuizzerNirajan <nbasnet183@gmail.com>
2020-10-11 18:00:41 +0545

    Initial draft for database diagram

--- covaplad/models.py
+++ covaplad/models.py
@@ -23,12 +23,22 @@ class User(db.Model, UserMixin):
     # !length assumed to be from werkzeug.security.generate_password_hash
     password = db.Column(su.PasswordType(schemes=["pbkdf2_sha512"]), nullable=False)
 
-    address = db.Column(db.String(255))
+    # address = db.Column(db.String(255))
     phone_number = db.Column(db.BigInteger)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
+    temporary_address_id = db.Column(db.Integer, db.ForeignKey("city.id"))
+    permanent_address_id = db.Column(db.Integer, db.ForeignKey("city.id"))
+
+    temporary_address = db.relationship(
+        "City", backref="temporary_residents", foreign_keys=temporary_address_id
+    )
+    permanent_address = db.relationship(
+        "City", backref="permanent_residents", foreign_keys=permanent_address_id
+    )
+
     """
-    DONOT load voluter info when loading User but load user info
+    DONOT load Volunteer info when loading User but load user info
     when loading Volunteer and Donor
     TODO: Check its uses and complexity later on
     # """
@@ -51,12 +61,20 @@ event_registration = db.Table(
     db.Column("event_id", db.Integer, db.ForeignKey("event.id"), primary_key=True),
 )
 
+volunteer_skills = db.Table(
+    "volunteer_skills",
+    db.Column(
+        "volunteer_id", db.Integer, db.ForeignKey("volunteer.id"), primary_key=True
+    ),
+    db.Column("skill_id", db.Integer, db.ForeignKey("skill.id"), primary_key=True),
+)
+
 
 class Volunteer(db.Model):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     start_date_time = db.Column(db.DateTime, nullable=False)
     end_date_time = db.Column(db.DateTime, nullable=False)
-
+    interested_fields = db.Column(db.String(100), nullable=False)
     """
     DONOT load events when loading Volunteer but load volunters info
     using subquery when loading Event
@@ -67,6 +85,16 @@ class Volunteer(db.Model):
         secondary=event_registration,
         backref=db.backref("volunteers", lazy="subquery"),
     )
+    """
+    Load skills immediately after loading Volunteer using subquery but do not
+    load volunteers info when loading Skill
+    """
+    skills = db.relationship(
+        "Skill",
+        secondary=volunteer_skills,
+        lazy="subquery",
+        backref=db.backref("volunteers", lazy=True),
+    )
 
     def __repr__(self):
         return (
@@ -75,12 +103,29 @@ class Volunteer(db.Model):
         )
 
 
-class Event(db.Model):
+class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    details = db.Column(db.Text(), nullable=False)
+    volunteer_id = db.Column(db.Integer, db.ForeignKey("volunteer.id"), nullable=True)
+
+
+class EventType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    events = db.relationship("Event", backref="eventtype", lazy=True)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
+
+    event_type_id = db.Column(
+        db.Integer, db.ForeignKey("event_type.id"), nullable=False
+    )
+    city_id = db.Column(db.Integer, db.ForeignKey("city.id"), nullable="False")
 
     def __repr__(self):
         return (
@@ -97,10 +142,20 @@ donation_registration = db.Table(
     ),
 )
 
+donor_disease = db.Table(
+    "donor_disease",
+    db.Column("donor_id", db.Integer, db.ForeignKey("donor.id"), primary_key=True),
+    db.Column("disease_id", db.Integer, db.ForeignKey("disease.id"), primary_key=True),
+)
+
 
 class Donor(db.Model):
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     covid_last_symptom_date = db.Column(db.Date, nullable=False)
+    blood_group = db.Column(db.String(10), nullable=False)
+    test_report = db.Column(db.Text())
+    age = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
 
     """
     DONOT load donation venues when loading Donor but load donors info
@@ -113,16 +168,70 @@ class Donor(db.Model):
         backref=db.backref("donors", lazy="subquery"),
     )
 
+    """
+    Load donor_diseases when loading Donor but dont load donors info
+    when loading diseases
+    TODO: Check its uses and complexity later on
+    """
+
+    diseases = db.relationship(
+        "Disease",
+        secondary=donor_disease,
+        lazy=False,
+        backref=db.backref("donors", lazy=True),
+    )
+
     def __repr__(self):
         return f"Donor(covid_last_symptom_date='{self.covid_last_symptom_date}')"
+
+
+class Disease(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    icd_code = db.Column(db.String(10), unique=True, nullable=False)
 
 
 class DonationVenue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(su.EmailType(), unique=True, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
     phone_number = db.Column(db.BigInteger, nullable=False)
+    city_id = db.Column(db.Integer, db.ForeignKey("city.id"), nullable=False)
 
     def __repr__(self):
         return f"DonationVenue(name='{self.name}', address='{self.address}')"
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    provinces = db.relationship("Province", backref="country", lazy=True)
+
+
+class Province(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey("country.id"), primary_key=True)
+
+    districts = db.relationship("District", backref="province", lazy=True)
+
+
+class District(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    province_id = db.Column(db.Integer, db.ForeignKey("province.id"), primary_key=True)
+
+    cities = db.relationship("City", backref="district", lazy=True)
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    district_id = db.Column(db.Integer, db.ForeignKey("district.id"), nullable=False)
+
+    venues = db.relationship("DonationVenue", backref="city", lazy=True)
+    events = db.relationship(
+        "Event",
+        backref="city",
+        lazy=True,
+    )
