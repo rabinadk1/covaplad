e2f75bce23795c13bee5eba1322e51cb56c9b363
rabinadk1 <rabin.adk1@gmail.com>
2020-10-17 22:49:45 +0545

    Use more efficient and readable method for overriding UserAdmin field

--- user/admin.py
+++ user/admin.py
@@ -3,33 +3,39 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+
 # Register your models here.
-
-_fieldsets = list(UserAdmin.fieldsets)
-
-_fieldsets.insert(
-    2,
-    (
-        "Extra Fields",
-        {
-            "fields": (
-                "phone_number",
-                "gender",
-                "dob",
-                "temporary_address",
-                "permanent_address",
-            )
-        },
-    ),
-)
-
-_fieldsets[1][1]["fields"] = (
-    "first_name",
-    "middle_name",
-    "last_name",
-    "email",
-)
-
-UserAdmin.fieldsets = _fieldsets
-
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "middle_name", "last_name", "email")},
+        ),
+        (
+            "Extra Fields",
+            {
+                "fields": (
+                    "phone_number",
+                    "gender",
+                    "dob",
+                    "temporary_address",
+                    "permanent_address",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
