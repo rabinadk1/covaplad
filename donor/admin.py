4e1b5f76e0d0b469ff5ab44e7f77443340920b56
rabinadk1 <rabin.adk1@gmail.com>
2020-10-17 20:32:29 +0545

    Register models to admin site

--- donor/admin.py
+++ donor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Disease, Donor
+
 # Register your models here.
+admin.site.register(Donor)
+admin.site.register(Disease)
--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Event, EventType
+
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(EventType)
