50701fc784d5fa59cd368e9410dc678dc61f2f39
Safal Thapaliya <safalthapaliya@gmail.com>
2020-11-07 21:28:50 +0545

    changed user creation form in admin site

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "middle_name", "last_name", "email"]
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ("email",)
 
 
 # Register your models here.
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
     fieldsets = (
         (None, {"fields": ("username", "password")}),
         (
@@ -38,4 +54,40 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {"fields": ("username", "password1", "password2", "email")}),
+        (
+            "Personal Information",
+            {
+                "classes": ("wide"),
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                    "phone_number",
+                    "gender",
+                    "dob",
+                ),
+            },
+        ),
+        (
+            "Address Information",
+            {
+                "fields": (
+                    "temporary_address",
+                    "permanent_address",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
