64a9b896d02dc6521a8da430f20a1d6e801becfd
Samip Poudel <074bct533.samip@pcampus.edu.np>
2020-12-15 21:30:47 +0545

    Event form added

--- event/views.py
+++ event/views.py
@@ -1,3 +1,23 @@
 # from django.shortcuts import render
 
 # Create your views here.
+# from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpRequest
+from django.shortcuts import render
+
+from . import models
+
+
+def get_event_list(request: HttpRequest):
+    events = models.Event.objects.all().order_by("name")
+    # print(events)
+    context = {"events": events}
+    return render(request, "events.html", context=context)
+
+
+def get_event(request: HttpRequest, venue_id):
+    event = models.Event.objects.get(id=venue_id)
+    context = {"event": event}
+    return render(request, "event.html", context=context)
