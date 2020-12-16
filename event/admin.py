b2e4417333d6bf0182468638c0899f292b681b99
QuizzerNirajan <nbasnet183@gmail.com>
2020-12-16 23:33:45 +0545

    added volunteer info on event and added link to the user page

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 
-from .models import Event, EventType
+from django.urls import reverse
+from django.utils.html import mark_safe
 
-# Register your models here.
-admin.site.register(Event)
+from .models import Event, EventType
+from volunteer.models import VolunteerRegistration
+
+
+class VolunteerRegistrationInline(admin.TabularInline):
+    model = VolunteerRegistration
+    extra = 0
+    readonly_fields = 'link',
+
+    def link(self, instance):
+        url = reverse("admin:volunteer_volunteer_change", args=(instance.id,))
+        return mark_safe("<a href='%s'>%s</a>" % (url, instance))
+
+    # show_change_link = True
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = (VolunteerRegistrationInline,)
+
+
+# Register your models here
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventType)
