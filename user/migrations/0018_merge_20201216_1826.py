a80a526ff661009c7fa7a520f51e514e104b776d
rabinadk1 <rabin.adk1@gmail.com>
2020-12-16 20:28:23 +0545

    Made Github actions and @thapaliya19 happy

--- user/migrations/0018_merge_20201216_1826.py
+++ user/migrations/0018_merge_20201216_1826.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0017_auto_20201216_1818'),
-        ('user', '0017_auto_20201216_1754'),
+        ("user", "0017_auto_20201216_1818"),
+        ("user", "0017_auto_20201216_1754"),
     ]
 
-    operations = [
-    ]
+    operations = []
